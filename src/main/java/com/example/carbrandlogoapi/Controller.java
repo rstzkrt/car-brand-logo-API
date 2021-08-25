e706d415c35167062dcdcc4f16ea0b45d00e5fb2
Resat Ozkurt <ozkurtresat@gmail.com>
2021-08-26 00:55:07 +0200

    add controller

---
+++ src/main/java/com/example/carbrandlogoapi/Controller.java
@@ -0,0 +1,24 @@
+package com.example.carbrandlogoapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @Autowired
+    private ModelService modelService;
+
+    @GetMapping("/model")
+    public ResponseEntity<List<Model>> getModels() throws IOException {
+
+        return ResponseEntity.ok(modelService.getModels());
+    }
+}
