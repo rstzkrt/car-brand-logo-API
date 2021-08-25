71103a5e714ed0b00837327e897b0c69ebe2d318
Resat Ozkurt <ozkurtresat@gmail.com>
2021-08-26 00:56:19 +0200

    add Entity

---
+++ src/main/java/com/example/carbrandlogoapi/Model.java
@@ -0,0 +1,14 @@
+package com.example.carbrandlogoapi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Model {
+    private String brand;
+    private String logo;
+
+}
