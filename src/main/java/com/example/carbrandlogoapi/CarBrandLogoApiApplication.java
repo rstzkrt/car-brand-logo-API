c27b7c85213c0a44f72bd648f1a7f0c0ed456c00
Resat Ozkurt <ozkurtresat@gmail.com>
2021-08-26 00:55:52 +0200

    refactor

---
+++ src/main/java/com/example/carbrandlogoapi/CarBrandLogoApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.carbrandlogoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBrandLogoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarBrandLogoApiApplication.class, args);
+    }
+}
